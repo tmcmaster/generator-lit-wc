b1532414149ee952112b03ee59eb88e8ee7d3b85
Tim McMaster <tim@mcmaster.id.au>
2019-05-29 09:00:58 +1000

    added support adding web components

--- generators/wc/index.js
+++ generators/wc/index.js
@@ -1,25 +1,55 @@
 const Generator = require('yeoman-generator');
 
+// noinspection JSUnresolvedFunction
+const common = require('../../lib/common.js');
+
+// noinspection JSUnresolvedVariable
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
 
-        this.defaultElementName = (this.appname === 'src' ? 'tm-element' : this.appname.replace(/\s+/g, '-'));
+        // noinspection JSUnresolvedVariable
+        const folderName = process.cwd().split('/').pop();
+        if (folderName === 'src') {
+            opts['ignore-folder'] = true;
+        }
+        this.elementNames = common.createElementNamesInitial(args, opts, folderName);
+        console.log('== Element Names: ', this.elementNames);
     }
 
+    // noinspection JSUnusedGlobalSymbols
     async prompting() {
-        const defaultElementName = this.defaultElementName;
-        self.answers = await self.prompt([
+        const elementNames = this.elementNames.join(' ');
+        this.answers = await this.prompt([
             {
                 type: 'input',
-                name: 'elementName',
-                message: 'Element Name',
-                default: defaultElementName
+                name: 'elementNames',
+                message: 'Element Names',
+                default: elementNames
             }
         ]);
     }
+    // noinspection JSUnusedGlobalSymbols
     writing() {
-        const elementName = this.answers.elementName;
-
+        this._processAnswers();
+        this._writeSrcFiles();
     }
-}
+
+    _processAnswers() {
+        this.elementNames = common.createElementNamesAnswers(this.answers.elementNames);
+        console.log('## Element Names: ', this.elementNames);
+    }
+
+    _writeSrcFiles() {
+        this.elementNames.forEach((elementName) => {
+            // noinspection JSUnresolvedFunction
+            this.fs.copyTpl(
+                this.templatePath('tm-element.js.template'),
+                this.destinationPath(elementName + '.js'), {
+                    elementName: elementName,
+                    className: common.dashToCamel(elementName, true)
+                }
+            );
+        });
+    }
+};
